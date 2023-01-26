7270ebbe44e44973312868142ab52f32a4c37be9
heaston2000 <heaston2000@gmail.com>
2023-01-26 17:35:13 -0500

    Working trainer and model, truncated data set

--- CNNAttempt1.py
+++ CNNAttempt1.py
@@ -1,4 +1,5 @@
 # This is meant to be the CNN for Breast Cancer Detection competition run by Kaggle and the Radiological Society of North America
+#   as an extension GradCAM is implemented on the dataset such that 
 
 import torch
 import torch.nn as nn
@@ -27,12 +28,12 @@ of each .csv and once we load in those, we go through them one batch at a time a
 class PatientMetaData():
     def __init__(self):
         self.all_meta_data  = pd.read_csv("train.csv")
-        
-        self.patient_image_ids = self.all_meta_data.iloc[:, 1:3] # Columns for patient ID, imageID, left "L" or right "R" breast
-        self.cancer_labels  = self.all_meta_data.iloc[:,6]
-        print(self.patient_image_ids)
+        # We only have 29,427 of the images 
+        self.patient_image_ids = self.all_meta_data.iloc[:29427, 1:3] # Columns for patient ID, imageID, left "L" or right "R" breast
+        self.cancer_labels  = self.all_meta_data.iloc[:29427,6]
+        #print(self.patient_image_ids)
         self.n_samples = len(self.patient_image_ids)
-        print(self.n_samples)
+        #print(self.n_samples)
         
     def __getitem__(self, index):
         # Return patient ID, image ID, cancer bool
@@ -67,27 +68,42 @@ data_loader   = DataLoader(dataset=meta_dataset, batch_size = batch_size, shuffl
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5) # Assuming images have 3 color channels, output size is 6 and kernels are 5x5
-        self.pool = nn.MaxPool2d(2,2) # Kernel size is 2x2 with stride of 2
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*5*5, 64)
-        self.fc2 = nn.Linear(64, 16)
-        self.fc3 = nn.Linear(16, 2)
+        self.block1 = nn.Sequential(
+            nn.Conv2d(1, 6, 5), # Assuming 1 in channel (inputs are b&w), output channel size is 6 and kernels are 5x5
+            nn.ReLU(),
+            nn.MaxPool2d(2,2), # Kernel size is 2x2 with stride of 2
+            nn.Conv2d(6, 16, 5), # input channel size is equal to the last channel size, kernels are still 5x5
+            nn.ReLU()
+        )
+        self.block2 = nn.Sequential(
+            nn.Linear(1000000, 64), #16 * 5*5
+            nn.ReLU(),
+            nn.Linear(64, 2)
+        )
+    # We need to apply the hook before the last pooling layer and after all the convolutional layers
     
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16*5*5) # Flatten so we can feed to fc layer
-        
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        
-        return x
-    
+        x_internal = self.block1(x) 
+        x_internal = torch.flatten(x_internal)
+        y_pred = self.block2(x_internal)
+        return y_pred
+   
+def onehot(c):
+    #Vector is one hot encoded with v1 = negative , v2 = positive
+    v = torch.zeros(2)
+    v[c] = 1
+    return v
+
+
+
+   
+   
+
 model = ConvNet()
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+transform = transforms.Compose([transforms.ToTensor()])
+#tensor_frog = transform(frog)
 
 
 
@@ -95,29 +111,48 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 # Images can be found here: "https://www.kaggle.com/datasets/radek1/rsna-mammography-images-as-pngs?select=images_as_pngs"
 num_steps = len(data_loader) # Need to implement the train loader depending on what the data is
 for epoch in range(num_epochs):
-    for i, (filepaths, cancer) in enumerate(data_loader):
+    total_loss = 0
+    for i, (filepaths, c) in enumerate(data_loader):
         #images = images.to(device)
         #labels = labels.to(device)
         
-        for filepath in filepaths:
-            print(filepath)
+        avg_batch_loss = 0
+        for j, filepath in enumerate(filepaths):
+            #print(filepath)
             # Image format is patientID_imageID.png
             current_mammo = Image.open(filepath)
-            print(np.array(current_mammo).shape)
+            image = np.array(current_mammo).shape
+            
             
         
+            image_tensor = transform(current_mammo)
+            #print(image_tensor.shape)
+            # Forward pass
+            output = model(image_tensor)
+            loss = criterion(output, onehot(c[j]))
+            avg_batch_loss += loss / len(filepath)
+        total_loss += avg_batch_loss
+        
         # Forward pass
-        outputs = model(images)
-        loss = criterion(outputs, labels)
+        #outputs = model(images)
+        #loss = criterion(outputs, labels)
+        
+        if i > 1000:
+            break
+        elif i % 200 == 0:
+            print(f'Step: {i}, Batch Loss: {avg_batch_loss}')
         
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+    
+    total_loss = total_loss / 1000
+    print(f'epoch: {epoch}, loss: {total_loss}')
         
         
 # Evaluate the model
-with torch.no_grad():
+with network.eval():
     total_correct = 0
     total_guessed = 0
     
